79e088bec207280d34591ffe886215a0f29e097a
jivco <jivco@users.noreply.github.com>
2017-03-09 16:39:44 +0200

    Add files via upload

--- hls_stress_tool.sh
+++ hls_stress_tool.sh
@@ -6,44 +6,47 @@
 #                                   #
 # ztodorov@neterra.net              #
 #                                   #
-# v.0.01                            #
+# v.0.02                            #
 #####################################
 
 CHUNKDURATION=5
-URL="http://localhost:1935/dvr/smil:$1.smil"
+URL="http://94.156.44.142:8080/dvr/smil:$1.smil"
 PLAYLIST="$URL/playlist.m3u8?DVR"
 
 while true
   do
     IFS=$'\r\n' GLOBIGNORE='*' command eval  "BITRATE_PLAYLISTS=($(curl -s $PLAYLIST |grep -v '#'))"
-    #echo "${BITRATE_PLAYLISTS[0]}"
-    #echo "${BITRATE_PLAYLISTS[1]}"
+    echo ""
+    echo "${BITRATE_PLAYLISTS[0]}"
+    echo "${BITRATE_PLAYLISTS[1]}"
 
     
     IFS=$'\r\n' GLOBIGNORE='*' command eval  "CHUNKLIST1=($(curl -s $URL/${BITRATE_PLAYLISTS[0]} |grep -v '#'))"
     IFS=$'\r\n' GLOBIGNORE='*' command eval  "CHUNKLIST2=($(curl -s $URL/${BITRATE_PLAYLISTS[1]} |grep -v '#'))"
 
-    echo ""
-    echo "$URL"
+    echo "$PLAYLIST"
     echo "${CHUNKLIST1[0]}"
     echo "${CHUNKLIST2[0]}"
     echo "${CHUNKLIST1[-1]}"
     echo "${CHUNKLIST2[-1]}"
 
-    #echo ${#CHUNKLIST1[@]}
-    #echo ${#CHUNKLIST2[@]}
+    LASTCH1=${#CHUNKLIST1[-1]}
+    LASTCH2=${#CHUNKLIST2[-1]}
 
     end=$((SECONDS+$CHUNKDURATION))
 
     while [ $SECONDS -lt $end ]
       do
 
-        CHUNK1=`shuf -i 0-${#CHUNKLIST1[@]} -n 1`
-        CHUNK2=`shuf -i 0-${#CHUNKLIST2[@]} -n 1`
+        CHUNK1=`shuf -i 0-$LASTCH1 -n 1`
+        CHUNK2=`shuf -i 0-$LASTCH2 -n 1`
 
-        curl -s -o /dev/null $URL/$CHUNK1
+        #echo $CHUNK1
+        #echo $CHUNK2
+
+        curl -s -o /dev/null "$URL/${CHUNKLIST1[$CHUNK1]}"
         echo -n "."
-        curl -s -o /dev/null $URL/$CHUNK2
+        curl -s -o /dev/null "$URL/${CHUNKLIST2[$CHUNK2]}"
         echo -n "."
 
 
@@ -51,3 +54,5 @@ while true
 
     
 done
+
+
