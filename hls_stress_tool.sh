8149d49fe9e07e89dd02f58d579262ed57064f31
jivco <jivco@users.noreply.github.com>
2017-03-10 14:10:01 +0200

    change logic to use curl's keepalive

--- hls_stress_tool.sh
+++ hls_stress_tool.sh
@@ -6,12 +6,12 @@
 #                                   #
 # ztodorov@neterra.net              #
 #                                   #
-# v.0.03                            #
+# v.0.04                            #
 #####################################
 
 CHUNKDURATION=5
-URL="http://localhost:8080/dvr/smil:$1.smil"
-PLAYLIST="$URL/playlist.m3u8?DVR"
+URL="http://94.156.44.142:8080/dvr/smil:$1.smil"
+PLAYLIST="$URL/playlist.m3u8?DVR&h=111222333"
 
 while true
   do
@@ -30,10 +30,10 @@ while true
     echo "${CHUNKLIST1[-1]}"
     echo "${CHUNKLIST2[-1]}"
 
-    FIRSTCH1=`echo ${CHUNKLIST1[0]}|awk -F "_" '{print $5}'|awk -F "." '{print $1}'`
-    FIRSTCH2=`echo ${CHUNKLIST2[0]}|awk -F "_" '{print $5}'|awk -F "." '{print $1}'`
-    LASTCH1=`echo ${CHUNKLIST1[-1]}|awk -F "_" '{print $5}'|awk -F "." '{print $1}'`
-    LASTCH2=`echo ${CHUNKLIST2[-1]}|awk -F "_" '{print $5}'|awk -F "." '{print $1}'`
+    FIRSTCH1=0
+    FIRSTCH2=0
+    LASTCH1=${#CHUNKLIST1[@]}
+    LASTCH2=${#CHUNKLIST2[@]}
 
     #echo $FIRSTCH1
     #echo $FIRSTCH2
@@ -45,20 +45,27 @@ while true
     while [ $SECONDS -lt $end ]
       do
 
-        CHUNK1=`shuf -i $FIRSTCH1-$LASTCH1 -n 1`
-        CHUNK2=`shuf -i $FIRSTCH2-$LASTCH2 -n 1`
+        CHUNKS1=''
+        CHUNKS2=''
 
-        echo -n $CHUNK1
-        curl -s -o /dev/null "$URL/${CHUNKLIST1[$CHUNK1]}"
-        echo -n "."
-        echo -n $CHUNK2
-        curl -s -o /dev/null "$URL/${CHUNKLIST2[$CHUNK2]}"
-        echo -n "."
+        for i in {1..10}
+        do
+          CHUNK1=`shuf -i $FIRSTCH1-$LASTCH1 -n 1 -z`
+          CHUNK2=`shuf -i $FIRSTCH2-$LASTCH2 -n 1 -z`
+          CHUNKS1="$CHUNKS1 $URL/${CHUNKLIST1[$CHUNK1]}"
+          CHUNKS2="$CHUNKS2 $URL/${CHUNKLIST2[$CHUNK2]}"
+        done
 
+
+        #echo $CHUNKS1
+        curl -s $CHUNKS1 >/dev/null 2>&1
+        echo -n "."
+        #echo $CHUNKS2
+        curl -s $CHUNKS2 >/dev/null 2>&1
+        echo -n "#"
 
     done
 
     
 done
-
 
