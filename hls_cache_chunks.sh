dea220c8213328be6fc3950a39e7ebb7384a34e7
jivco <jivco@users.noreply.github.com>
2017-03-22 15:46:33 +0200

    Update hls_cache_chunks.sh

--- hls_cache_chunks.sh
+++ hls_cache_chunks.sh
@@ -8,42 +8,38 @@
 # v.0.03                            #
 #####################################
 
-USAGE_HELP='Usage: hls_cache_chunks.sh bnt1 127.0.0.1'
-
-if [ -z ${1+x} ]; then 
-  echo "channel name is unset";
-  echo $USAGE_HELP;
-  exit;
-fi
-
-if [ -z ${2+x} ]; then 
-  echo "wowza ip is unset";
-  echo $USAGE_HELP;
-  exit;
-fi
-
-WOW_URL="http://$2:1935/dvr/$1.smil"
-WOW_PLAYLIST="$WOW_URL/playlist.m3u8"
+WOW_APP='dvr'
+SMIL='smil'
+NGX_APP_DIR=$WOW_APP/$1.$SMIL
+WOW_URL="http://$2:1935/$WOW_APP/$1.$SMIL"
+WOW_PLAYLIST=$WOW_URL'/playlist.m3u8'
 BITRATE1='360p'
+RESOLUTION1='640x360'
+BANDWIDTH1='764000'
 BITRATE2='480p'
-TMP_STORE='/tmp/tmp'
-NGX_ROOT='/tmp/tmp'
-NGX_BITRATE1=$NGX_ROOT/$1_$BITRATE1.m3u8
-NGX_BITRATE2=$NGX_ROOT/$1_$BITRATE2.m3u8
-NGX_PLAYLIST1=$TMP_STORE/$2.$1.$BITRATE1.tmpl
-NGX_PLAYLIST2=$TMP_STORE/$2.$1.$BITRATE2.tmpl
-NGX_BITRATE1_TMP=$TMP_STORE/$2.$1.$BITRATE1.tmp
-NGX_BITRATE2_TMP=$TMP_STORE/$2.$1.$BITRATE2.tmp
+RESOLUTION2='854x480'
+BANDWIDTH2='1596000'
+TMP_STORE='/tmp'
+NGX_ROOT='/mnt/store/nginx_root'
+NGX_BITRATE_URL1=$1'_'$BITRATE1'.m3u8'
+NGX_BITRATE_URL2=$1'_'$BITRATE2'.m3u8'
+NGX_BITRATE1=$NGX_ROOT'/'$NGX_APP_DIR'/'$NGX_BITRATE_URL1
+NGX_BITRATE2=$NGX_ROOT'/'$NGX_APP_DIR'/'$NGX_BITRATE_URL2
+NGX_PLAYLIST1=$TMP_STORE'/'$WOW_APP.$2.$1.$BITRATE1'.tmpl'
+NGX_PLAYLIST2=$TMP_STORE'/'$WOW_APP.$2.$1.$BITRATE2'.tmpl'
+NGX_BITRATE1_TMP=$TMP_STORE'/'$WOW_APP.$2.$1.$BITRATE1'.tmp'
+NGX_BITRATE2_TMP=$TMP_STORE'/'$WOW_APP.$2.$1.$BITRATE2'.tmp'
+NGX_MAIN_PLAYLIST_TMP=$TMP_STORE'/'$WOW_APP.$2.$1.playlist'.tmp'
+NGX_MAIN_PLAYLIST=$NGX_APP_DIR'/playlist.m3u8'
 # in seconds
 CHUNKDURATION=5
 # in hours
 DVR_WINDOW=8
 MAXCHUNKS=$(($DVR_WINDOW*3600/$CHUNKDURATION))
 MAXLINES=$((2*$MAXCHUNKS))
+USAGE_HELP='Usage: hls_cache_chunks.sh bnt1 127.0.0.1'
 
-rm $NGX_PLAYLIST1 $NGX_PLAYLIST2 $NGX_BITRATE1_TMP  $NGX_BITRATE2_TMP
-
-function gen_playlist {
+function gen_chunklist {
     if [ "$(cat $NGX_PLAYLIST|wc -l)" -eq "0" ]; then
        touch $NGX_PLAYLIST;
     fi
@@ -52,8 +48,8 @@ function gen_playlist {
        # Remove first 2 rows of playlist - this is how playlist rotation is made
        tail -n +2 $NGX_PLAYLIST>$NGX_PLAYLIST;
     fi
-    
-    NGX_CHUNK=($(tail -n 1 $NGX_PLAYLIST)) 
+
+    NGX_CHUNK=($(tail -n 1 $NGX_PLAYLIST))
 
     if [ "${WOW_CHUNKLIST[-1]}" != "$NGX_CHUNK" ]; then
       curl -s -o /dev/null $WOW_URL/${WOW_CHUNKLIST[-1]} >/dev/null 2>&1
@@ -61,7 +57,7 @@ function gen_playlist {
       echo ${WOW_CHUNKLIST[-1]} >>$NGX_PLAYLIST;
     fi
 
-    XMS=$(head $NGX_PLAYLIST -n2|grep -v "#"|awk -F "_" '{print $5}'|awk -F "." '{print $1}')
+    XMS=$(head $NGX_PLAYLIST -n2|grep -v "#"|awk -F "_" '{print $4}'|awk -F "." '{print $1}')
 
     echo '#EXTM3U' >$NGX_BITRATE_TMP
     echo '#EXT-X-VERSION:3' >>$NGX_BITRATE_TMP
@@ -73,9 +69,33 @@ function gen_playlist {
     mv $NGX_BITRATE_TMP $NGX_BITRATE
 }
 
+if [ -z ${1+x} ]; then
+  echo "channel name is unset";
+  echo $USAGE_HELP;
+  exit;
+fi
+
+if [ -z ${2+x} ]; then
+  echo "wowza ip is unset";
+  echo $USAGE_HELP;
+  exit;
+fi
+
+rm $NGX_PLAYLIST1 $NGX_PLAYLIST2 $NGX_BITRATE1_TMP  $NGX_BITRATE2_TMP
+
+mkdir -p $NGX_ROOT/$NGX_APP_DIR
+echo '#EXTM3U'>$NGX_MAIN_PLAYLIST_TMP
+echo '#EXT-X-VERSION:3'>>$NGX_MAIN_PLAYLIST_TMP
+echo "#EXT-X-STREAM-INF:BANDWIDTH=$BANDWIDTH1,RESOLUTION=$RESOLUTION1">>$NGX_MAIN_PLAYLIST_TMP
+echo "$NGX_BITRATE_URL1">>$NGX_MAIN_PLAYLIST_TMP
+echo "#EXT-X-STREAM-INF:BANDWIDTH=$BANDWIDTH2,RESOLUTION=$RESOLUTION2">>$NGX_MAIN_PLAYLIST_TMP
+echo "$NGX_BITRATE_URL2">>$NGX_MAIN_PLAYLIST_TMP
+
+mv $NGX_MAIN_PLAYLIST_TMP $NGX_MAIN_PLAYLIST
+
 while true
   do
-    IFS=$'\r\n' GLOBIGNORE='*' command eval  "BITRATE_WOW_PLAYLISTS=($(curl -s -sH 'Accept-encoding: gzip' --compressed $WOW_PLAYLIST |grep -v '#'))"
+    IFS=$'\r\n' GLOBIGNORE='*' command eval "BITRATE_WOW_PLAYLISTS=($(curl -s -sH 'Accept-encoding: gzip' --compressed $WOW_PLAYLIST |grep -v '#'))"
     IFS=$'\r\n' GLOBIGNORE='*' command eval "WOW_CHUNKLIST1=($(curl -s -sH 'Accept-encoding: gzip' --compressed $WOW_URL/${BITRATE_WOW_PLAYLISTS[0]} |grep -v '#'))"
     IFS=$'\r\n' GLOBIGNORE='*' command eval "WOW_CHUNKLIST2=($(curl -s -sH 'Accept-encoding: gzip' --compressed $WOW_URL/${BITRATE_WOW_PLAYLISTS[1]} |grep -v '#'))"
 
@@ -84,16 +104,15 @@ while true
     WOW_CHUNKLIST=("${WOW_CHUNKLIST1[@]}")
     NGX_BITRATE_TMP=$NGX_BITRATE1_TMP
     NGX_BITRATE=$NGX_BITRATE1
-    gen_playlist
+    gen_chunklist
 
     NGX_PLAYLIST=$NGX_PLAYLIST2
     NGX_CHUNK=$NGX_CHUNK2
     WOW_CHUNKLIST=("${WOW_CHUNKLIST2[@]}")
     NGX_BITRATE_TMP=$NGX_BITRATE2_TMP
     NGX_BITRATE=$NGX_BITRATE2
-    gen_playlist
-    
-    sleep 1
-    
-done
+    gen_chunklist
 
+    sleep 1
+
+done
