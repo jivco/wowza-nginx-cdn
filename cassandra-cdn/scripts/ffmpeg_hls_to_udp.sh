d39fa33d8e4d8fd24d58d212f8311053aa65a43e
Jivko Todorov <jivco@users.noreply.github.com>
2017-05-29 12:42:36 +0300

    Update ffmpeg_hls_to_udp.sh

--- cassandra-cdn/scripts/ffmpeg_hls_to_udp.sh
+++ cassandra-cdn/scripts/ffmpeg_hls_to_udp.sh
@@ -1,1 +1,1 @@
-ffmpeg -v debug -fflags +sortdts+bitexact+genpts+igndts+discardcorrupt -re -i http://172.16.21.1/dvr/bnt1_480p_playlist_all.m3u8 -c copy -f mpegts 'udp://172.16.21.3:33000?pkt_size=1316'
+ffmpeg -v debug -report -fflags +sortdts+bitexact+genpts+igndts+discardcorrupt -re -i http://172.16.21.1/dvr/bnt1_480p_playlist_all.m3u8 -c copy -f mpegts 'udp://172.16.21.3:33000?pkt_size=1316'
