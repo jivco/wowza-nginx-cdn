2081e8ddaf89a02e76592bdd18fbcd49e40e4571
Jivko Todorov <jivco@users.noreply.github.com>
2017-06-06 11:06:57 +0300

    Update ffmpeg_hls_to_udp.sh

--- cassandra-cdn/scripts/ffmpeg_hls_to_udp.sh
+++ cassandra-cdn/scripts/ffmpeg_hls_to_udp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-while true; do FFREPORT=file=/tmp/ffreport.log ffmpeg -fflags +sortdts+bitexact+genpts+igndts+discardcorrupt -re -i http://172.16.21.1/dvr/bbc_world/1428000/60/playlist.m3u8 -c copy -f mpegts 'udp://127.0.0.1:30000?pkt_size=1316';done
+((FFREPORT=file=/tmp/ffreport.log ffmpeg -fflags +sortdts+bitexact+genpts+igndts+discardcorrupt -re -i http://172.16.21.1/dvr/bbc_world/1428000/60/playlist.m3u8 -c copy -f mpegts 'udp://127.0.0.1:30000?pkt_size=1316') & echo $! > /var/run/ffmpeg.pid &)
