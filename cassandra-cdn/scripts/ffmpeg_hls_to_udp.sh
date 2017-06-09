57ab156e70aab16fef2e4883396cb14894893f67
Jivko Todorov <jivco@users.noreply.github.com>
2017-06-09 12:40:50 +0300

    Create ffmpeg_hls_to_udp.sh

--- cassandra-cdn/scripts/ffmpeg_hls_to_udp.sh
+++ cassandra-cdn/scripts/ffmpeg_hls_to_udp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-((FFREPORT=file=/tmp/ffreport.log ffmpeg -fflags +sortdts+bitexact+genpts+igndts+discardcorrupt -re -i http://172.16.21.1/dvr/bbc_world/1428000/60/playlist.m3u8 -c copy -f mpegts 'udp://127.0.0.1:30000?pkt_size=1316') & echo $! > /var/run/ffmpeg.pid &)
+((FFREPORT=file=/tmp/ffreport.log /root/bin/ffmpeg -re -i http://172.16.21.1/dvr/bbc_world/1428000/60/chunklist.m3u8 -c copy -f mpegts 'udp://127.0.0.1:30000?pkt_size=1316' >/dev/null 2>/dev/null) & echo $! > /var/run/ffmpeg.pid &)
