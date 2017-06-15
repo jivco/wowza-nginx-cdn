73de9d0b3ae2f70b1ea513fe96f0975cd8e9482a
Jivko Todorov <jivco@users.noreply.github.com>
2017-06-15 10:58:05 +0300

    Create check_ffmpeg.sh

--- cassandra-cdn/scripts/check_ffmpeg.sh
+++ cassandra-cdn/scripts/check_ffmpeg.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+RESTART=0
+
 tail -n50 /tmp/ffreport.log|egrep '(DTS|expired|Bad|5XX)'
 
 if [ $? -eq 0 ]
+then
+  RESTART=1
+fi
+
+tail -n500 /tmp/ffreport.log|egrep '(\.ts)'
+
+if [ $? -eq 1 ]
+then
+  RESTART=1
+fi
+
+if [ "$RESTART" -eq "1" ]
 then
    kill -9 `cat /var/run/ffmpeg.pid`
    /root/ffmpeg-hls-to-udp.sh
