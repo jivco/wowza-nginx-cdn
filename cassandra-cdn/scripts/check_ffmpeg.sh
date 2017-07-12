06b3a1051a29211e534883f93e9cb8fd97b23744
Jivko Todorov <jivco@users.noreply.github.com>
2017-07-12 12:26:21 +0300

    Update check_ffmpeg.sh

--- cassandra-cdn/scripts/check_ffmpeg.sh
+++ cassandra-cdn/scripts/check_ffmpeg.sh
@@ -2,21 +2,21 @@
 
 RESTART=0
 
-tail -n50 /tmp/ffreport.log|egrep '(DTS|expired|Bad|5XX|Delay between the first packet and last packet in the muxing queue is|Invalid data)'
+tail -n50 /tmp/ffreport15.log|egrep '(DTS|expired|Bad|5XX|Delay between the first packet and last packet in the muxing queue is|Invalid data)'
 
 if [ $? -eq 0 ]
 then
   RESTART=1
 fi
 
-tail -n500 /tmp/ffreport.log|egrep '(\.ts)'
+tail -n500 /tmp/ffreport15.log|egrep '(\.ts)'
 
 if [ $? -eq 1 ]
 then
   RESTART=1
 fi
 
-tail /var/log/nss/nssd.log|grep Timeout|grep "`date --date='1 minutes ago' +"%Y/%m/%d %H:%M"`"|grep "'a'"
+tail -n500 /var/log/nss/nssd.log|grep Timeout|grep "`date --date='1 minutes ago' +"%Y/%m/%d %H:%M"`"|grep "'btv-sd2'"
 
 if [ $? -eq 0 ]
 then
@@ -25,7 +25,7 @@ fi
 
 if [ "$RESTART" -eq "1" ]
 then
-   kill -9 `cat /var/run/ffmpeg.pid`
-   /root/ffmpeg-hls-to-udp.sh
-   echo "`date` - fmpeg restarted">>/tmp/check_ffmpeg.log
+   kill -9 `cat /var/run/ffmpeg15.pid`
+   /root/ffmpeg-hls-to-udp-btv.sh
+   echo "`date` - fmpeg-btv restarted">>/tmp/check_ffmpeg.log
 fi
