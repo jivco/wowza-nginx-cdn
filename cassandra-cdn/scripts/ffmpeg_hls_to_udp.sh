7112b29e4cfb7f83d090690a47f83f6d7f89ba2d
Jivko Todorov <jivco@users.noreply.github.com>
2017-07-13 10:59:38 +0300

    Update ffmpeg_hls_to_udp.sh

--- cassandra-cdn/scripts/ffmpeg_hls_to_udp.sh
+++ cassandra-cdn/scripts/ffmpeg_hls_to_udp.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-((FFREPORT=file=/tmp/ffreport15.log /root/bin/ffmpeg -re -i http://172.16.21.1/dvr/btv/1428000/60/chunklist.m3u8 -c copy -f mpegts 'udp://127.0.0.1:30015?pkt_size=188&buffer_size=65535' >/dev/null 2>/dev/null) & echo $! > /var/run/ffmpeg15.pid &)
+TV=btv
+PORT=30015
+BITRATE=1428000
+APP=dvr
+OFFSET=60
 
+SUFF=$APP.$TV.$BITRATE.$OFFSET
+
+FIFO=/tmp/ffreport-$SUFF.fifo
+LOG=/tmp/ffreport-$SUFF.log
+PID=/var/run/ffmpeg-$SUFF.pid
+
+mkfifo $LOG
+
+((FFREPORT=file=$FIFO /root/bin/ffmpeg -re -i http://172.16.21.1/$APP/$TV/$BITRATE/$OFFSET/chunklist.m3u8 -c copy -f mpegts "udp://127.0.0.1:$PORT?pkt_size=1316" >/dev/null 2>/dev/null) & echo $! > $PID &)
+
+cat < $FIFO >>$LOG
