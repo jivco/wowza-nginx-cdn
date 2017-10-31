69304c54f69185bbcf9a13909d4d7329a23dd58a
Jivko Todorov <jivco@users.noreply.github.com>
2017-10-31 10:34:28 +0200

    Update gstreamer.sh

--- cassandra-cdn/scripts/gstreamer.sh
+++ cassandra-cdn/scripts/gstreamer.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gst-launch-1.0 -v souphttpsrc location=http://example.net/playlist.m3u8 ! hlsdemux ! tsdemux ! mpegtsmux ! udpsink port=5000 host=127.0.0.1
+gst-launch-1.0 -v souphttpsrc location=http://127.0.0.1:8000/stream/1080p.m3u8 ! hlsdemux ! tsdemux name=demux \
+demux. ! queue ! aacparse ! mpegtsmux name=mux ! udpsink port=30000 host=239.255.1.2 \
+demux. ! queue ! h264parse ! mux.
